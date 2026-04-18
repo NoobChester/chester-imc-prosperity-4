f2ed623eb8ca0a26efbcc59b092d74fd6020c1d5
NoobChester <chesterau@gmail.com>
2026-04-18 17:50:18 +1000

    written report on all possibilities of the amount we spend on scale and research

---
+++ round_2_scripts/manual_trade/list_research_scale.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+"""Model and maximize the Manual Trading challenge objective.
+
+Challenge objective:
+PnL = (research(x) * scale(y) * speed(z)) - Budget_Used
+We determine the suitable ratio between Research and Scale in this script.
+Speed is not considered at this time.
+Note that x,y,z represent the percentage of the budget to allocate
+
+Where:
+- research(x) = 200000 * log(1 + x) / log(101)
+- scale(y) = 7 * y
+- 0.1 <= speed(z) <= 0.9 (linearly based on player rank)
+- x >= 0, y >= 0, z >= 0
+- budget = 50_000
+- budget_used = budget * (x + y + z) / 100
+- 0 <= budget_used <= budget
+"""
+
+from __future__ import annotations
+
+import argparse
+import numpy as np
+from dataclasses import dataclass
+
+TOTAL_BUDGET = 50_000
+RESEARCH_MAX = 200_000
+SCALE_MAX = 7
+SPEED_MIN = 0.1
+SPEED_MAX = 0.9
+TOTAL_MAX = 100
+
+@dataclass
+class result:
+    research_percentage: int
+    scale_percentage: int
+    max_speed_pnl: int
+    min_speed_pnl: int
+
+"""Challenge formula: 200000 * log(1 + x) / log(101)."""
+def research_outcome(research: int) -> float:
+    return RESEARCH_MAX * np.log(1 + research) / np.log(1 + TOTAL_MAX)
+
+"""Linear growth from 0 to 7."""
+def scale_outcome(scale: int) -> float:
+    return SCALE_MAX * (scale / TOTAL_MAX)
+
+"""Used budget in XIRECs from percentage allocations."""
+def budget_used(research: int, scale: int) -> int:
+    return TOTAL_BUDGET * (research + scale) / TOTAL_MAX
+
+"""Compute challenge PnL."""
+def pnl(research: int, scale: int, speed: float) -> int:
+    return int(research_outcome(research) * scale_outcome(scale) * speed - budget_used(research, scale))
+
+def main() -> None:
+
+    step = 1
+    for research_scale_sum_max in range(0, 101, step):
+        best_max_speed_pnl = result(0, 0, -TOTAL_BUDGET, -TOTAL_BUDGET)
+        best_min_speed_pnl = result(0, 0, -TOTAL_BUDGET, -TOTAL_BUDGET)
+        for research in range(0, research_scale_sum_max + 1, step):
+            for scale in range(0, research_scale_sum_max + 1, step):
+                if (research + scale) > research_scale_sum_max:
+                    continue
+                current_max_speed_pnl = pnl(research, scale, SPEED_MAX)
+                current_min_speed_pnl = pnl(research, scale, SPEED_MIN)
+                if current_max_speed_pnl > best_max_speed_pnl.max_speed_pnl:
+                    best_max_speed_pnl = result(research, scale, current_max_speed_pnl, current_min_speed_pnl)
+                if current_min_speed_pnl > best_min_speed_pnl.min_speed_pnl:
+                    best_min_speed_pnl = result(research, scale, current_max_speed_pnl, current_min_speed_pnl)
+        print(f"Given max = {research_scale_sum_max}%, best alloc = {best_min_speed_pnl.research_percentage}% R {best_min_speed_pnl.scale_percentage}% S, PnL at min speed = {best_min_speed_pnl.min_speed_pnl}, PnL at max speed = {best_min_speed_pnl.max_speed_pnl}")
+
+if __name__ == "__main__":
+    main()
--- round_2_scripts/manual_trade/optimize_research_scale_findings.md
+++ round_2_scripts/manual_trade/optimize_research_scale_findings.md
@@ -15,3 +15,29 @@ PnL at max speed = 618096
 PnL at min speed = 24232
 
 We can infer that if we spend 0 on speed, 23 and 77 is the best way to spend on research and scale.
+
+We further expand the previous code:
+
+py list_research_scale.py
+
+Given max = 0%, best alloc = 0% R 0% S, PnL at min speed = 0, PnL at max speed = 0
+
+Given max = 1%, best alloc = 0% R 0% S, PnL at min speed = 0, PnL at max speed = 0
+
+...
+
+Given max = 30%, best alloc = 0% R 0% S, PnL at min speed = 0, PnL at max speed = 0
+
+Given max = 31%, best alloc = 0% R 0% S, PnL at min speed = 0, PnL at max speed = 0
+
+Given max = 32%, best alloc = 9% R 23% S, PnL at min speed = 65, PnL at max speed = 128587
+
+Given max = 33%, best alloc = 9% R 24% S, PnL at min speed = 263, PnL at max speed = 134374
+
+...
+
+Given max = 99%, best alloc = 23% R 76% S, PnL at min speed = 23768, PnL at max speed = 609920
+
+Given max = 100%, best alloc = 23% R 77% S, PnL at min speed = 24232, PnL at max speed = 618096
+
+We can infer that the more we allocate on research and speed, the better, as it outgrows the budget.
